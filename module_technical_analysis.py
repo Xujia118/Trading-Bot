d13f5ffa168dbbcf8db51b68b29b00730fb373df
Jia XU <xujia118@hotmail.com>
2023-07-07 15:33:56 +0800

    Update consolidation logic

--- module_technical_analysis.py
+++ module_technical_analysis.py
@@ -32,40 +32,45 @@ class TechnicalAnalysis:
         self.df['Boll_sell'] = np.where(self.df['High'] * (1 + self.boll_tolerance) >= self.df['Upper'], True, False)
         
         return self.df
-
+    
     def sell_consolidation(self):
-        shifted_df = self.df.shift(1, fill_value=0)    
+        yesterday_df = self.df.shift(1, fill_value=0)    
 
-        # yesterday was a small candle
-        small_candle = abs(shifted_df['Open'] - shifted_df['Close']) <= self.small_candle_tolerance * shifted_df['Open']
+        today_small_bearish_candle = (abs(self.df['Open'] - self.df['Close']) <= self.small_candle_tolerance * self.df['Open']) & \
+                                    self.df['Close'] <= self.df['Open']
+
+        # Bearish engulf
+        bearish_engulf = (self.df['Open'] * (1 + self.consolidation_tolerance) >= np.maximum(yesterday_df['Open'], yesterday_df['Close'])) & \
+                        (self.df['Close'] * (1 - self.consolidation_tolerance) <= np.minimum(yesterday_df['Open'], yesterday_df['Close']))
 
         # Flag
-        flag = (shifted_df['High'] * (1 + self.consolidation_tolerance) >= np.maximum(self.df['Open'], self.df['Close'])) & \
-                    (shifted_df['Low'] * (1 - self.consolidation_tolerance) <= np.minimum(self.df['Open'], self.df['Close']))
+        flag = (yesterday_df['High'] * (1 + self.consolidation_tolerance) >= np.maximum(self.df['Open'], self.df['Close'])) & \
+                    (yesterday_df['Low'] * (1 - self.consolidation_tolerance) <= np.minimum(self.df['Open'], self.df['Close']))
         
-        self.df['sell_consolidation'] = small_candle & flag                                          
+        self.df['sell_consolidation'] = bearish_engulf | (flag & today_small_bearish_candle)                                  
 
         return self.df
     
     def buy_consolidation(self):
-        shifted_df = self.df.shift(1, fill_value=0)
-
-        # yesterday was a small candle
-        small_candle = abs(shifted_df['Open'] - shifted_df['Close']) <= self.small_candle_tolerance * shifted_df['Open']
+        yesterday_df = self.df.shift(1, fill_value=0)
+        
+        today_small_candle = (abs(self.df['Open'] - self.df['Close']) <= self.small_candle_tolerance * self.df['Open']) #& \
+                                    #self.df['Close'] > self.df['Open']
         
         # Bullish engulf
-        bullish_engulf = (self.df['Close'] * (1 + self.consolidation_tolerance) >= np.maximum(shifted_df['Open'], shifted_df['Close'])) & \
-                        (self.df['Open'] * (1 - self.consolidation_tolerance) <= np.minimum(shifted_df['Open'], shifted_df['Close']))
+        bullish_engulf = (self.df['Close'] * (1 + self.consolidation_tolerance) >= np.maximum(yesterday_df['Open'], yesterday_df['Close'])) & \
+                        (self.df['Open'] * (1 - self.consolidation_tolerance) <= np.minimum(yesterday_df['Open'], yesterday_df['Close']))
         
         # Flag
-        flag = (shifted_df['High'] * (1 + self.consolidation_tolerance) >= np.maximum(self.df['Open'], self.df['Close'])) & \
-                    (shifted_df['Low'] * (1 - self.consolidation_tolerance) <= np.minimum(self.df['Open'], self.df['Close']))
+        flag = (yesterday_df['High'] * (1 + self.consolidation_tolerance) >= np.maximum(self.df['Open'], self.df['Close'])) & \
+                    (yesterday_df['Low'] * (1 - self.consolidation_tolerance) <= np.minimum(self.df['Open'], self.df['Close'])) 
+    
 
-        self.df['buy_consolidation'] = (small_candle & bullish_engulf) | flag
+        self.df['buy_consolidation'] = bullish_engulf | (flag & today_small_candle)
 
         return self.df
 
-    def good_to_buy(self, consolidation_days=5):  
+    def good_to_buy(self, consolidation_days=3):  
         self.df = self.SMA60()  
         self.df = self.SMA120() 
         self.df = self.Bollinger()         
@@ -90,10 +95,11 @@ class TechnicalAnalysis:
         
         return self.df
 
-    def good_to_sell(self, consolidation_days=3):
+    def good_to_sell(self, consolidation_days=0):
         self.df = self.Bollinger()
         self.df = self.sell_consolidation()
 
+        # Consolidation signal remains valid for three days
         boll_sell_dates = self.df.index[self.df['Boll_sell']].to_list()
         sell_status = {date: False for date in self.df.index}
 
--- parameters.py
+++ parameters.py
@@ -1,9 +1,9 @@
 boll_tolerance = 0.012
-small_candle_tolerance = 0.005
+small_candle_tolerance = 0.007
 consolidation_tolerance = 0.005
 SMA_tolerance = 0.1
 
 total_equity = 100000
 invest_ratio = 0.2
-rebuy_tolerance = 0.87
+rebuy_tolerance = 0.88
 profit_threshold = 0.10
