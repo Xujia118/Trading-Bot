f2022bba6c213ffb85c381b85594fed0a2e4d73d
Jia XU <xujia118@hotmail.com>
2023-07-21 07:54:04 +0800

    Fixed a candidate order bug

--- module_decide_candidates.py
+++ module_decide_candidates.py
@@ -1,18 +1,9 @@
-from alpaca.trading.client import TradingClient
-import config
 import module_scan_account
 import module_scan_candidates
 from module_order import Order
 import pandas as pd
 
 def decide_candidates():
-    tc = TradingClient(config.API_KEY, config.SECRET_KEY, paper=True)
-
-    # The bot would fire the same orders if yesterday was a holiday. 
-    # So if there are pending orders, do nothing. 
-    open_orders = tc.get_orders()
-    if len(open_orders) > 0:
-        return
 
     # Get available equity
     positions, num_positions, available_cash, total_equity = module_scan_account.scan_account()
