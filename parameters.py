7fd79c86903edf2d545ea93e732b8f0c82a0ab2d
Jia XU <xujia118@hotmail.com>
2023-07-21 22:34:53 +0800

    Adjusted rebuy_tolerance

--- parameters.py
+++ parameters.py
@@ -1,9 +1,9 @@
 boll_tolerance = 0.012
 small_candle_tolerance = 0.007
 consolidation_tolerance = 0.002
-SMA_tolerance = 0.1
+SMA_tolerance = 0.1 # 能防止在下跌行情中购买，但也抓不住最低谷发起的反转
 
 total_equity = 100000
 invest_ratio = 0.2
-rebuy_tolerance = 0.88
+rebuy_tolerance = 0.85 # 太高很快就二次吃进，太低则有可能吃不进，暂定0.85
 profit_threshold = 0.10
