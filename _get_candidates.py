041a6b1d7beb7ea3caf407e1f27804514962fc79
Jia XU <xujia118@hotmail.com>
2023-08-08 15:33:14 +0800

    Update get_candidates logic

--- _get_candidates.py
+++ _get_candidates.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv('Fundamentals_Nasdaq.csv', index_col='symbol')
-filt = (df['trailingEps'] >= df['trailingEps'].median()) & \
-        (df['returnOnEquity'] >= df['returnOnEquity'].median()) & \
-        (df['profitMargins'] >= df['profitMargins'].median())
+filt = (df['trailingEps'] >= 0) & \
+        (df['returnOnEquity'] >= 0) & \
+        (df['profitMargins'] >= 0)
 df = df.loc[filt]
 
 df.to_csv('candidates_Nasdaq.csv')
