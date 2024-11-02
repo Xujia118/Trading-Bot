2eeddad940d277e023210c33878f8e9e5193357a
Jia XU <xujia118@hotmail.com>
2024-11-02 09:52:51 -0700

    Reverted email_report.py

--- email_report.py
+++ email_report.py
@@ -3,11 +3,17 @@ from email_content import email_content
 import decide_positions 
 import decide_candidates
 from datetime import date
+import config
 
 def send_email():
     # Call functions to get plans for positions and other stocks
     positions_sell, positions_buy = decide_positions.decide_positions_actions()
     buy_plan = decide_candidates.decide_candidates() 
+
+    # Email configuration
+    sender_email = config.sender_email
+    receiver_email = '773977192@qq.com'
+    password = config.password
 
     # Formatting the report
     if len(positions_sell) == 0:
@@ -26,15 +32,19 @@ def send_email():
         buy_plan_str = '\n'.join([f"Ticker: {ticker}, Quantity: {quantity}" for ticker, price, quantity in buy_plan])    
 
     # Email content
-    report = email_content.format(
+    email_text = email_content.format(
         position_sell = position_sell_plan,
         position_buy = position_buy_plan,
         buy_plans = buy_plan_str,
         date=date.today()
     )
 
-    report = f'Subject: Stocks Report\n\n{report}'
-    print(report)
+    email_text = f'Subject: Stocks Report\n\n{email_text}'
+    print(email_text)
+
+    with smtplib.SMTP_SSL('smtp.163.com', 465) as server:
+        server.login(sender_email, password)
+        # server.sendmail(sender_email, receiver_email, email_text)
 
 if __name__ == '__main__':
     send_email()
